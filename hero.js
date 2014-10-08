244bd60ef393fc1a7f1eef78c2fd5811d3e64e51
Peter Beverwyk <pete@bwyk.com>
2014-10-07 21:59:54 -0500

    Careful assassin that'll grab a mine if no enemies are close

--- hero.js
+++ hero.js
@@ -2,10 +2,37 @@
 // // This hero will attempt to kill the closest weaker enemy hero.
 var move = function(gameData, helpers) {
    var myHero = gameData.activeHero;
+
+   var enemyStats = helpers.findNearestObjectDirectionAndDistance(gameData.board, myHero, function(boardTile) {
+     if (boardTile.type === 'Hero' && boardTile.team != myHero.team) {
+       return true;
+     }
+   });
+   var distanceToEnemy = enemyStats.distance;
+   var directionToEnemy = enemyStats.direction;
+
+   weakEnemy = helpers.findNearestWeakerEnemy(gameData);
+   nonTeamDiamondMine = helpers.findNearestNonTeamDiamondMine(gameData);
+   healthWell = helpers.findNearestHealthWell(gameData);
+   teamMember = helpers.findNearestTeamMember(gameData);
+   
    if (myHero.health < 50) {
-     return helpers.findNearestHealthWell(gameData);
-   } else {
-     return helpers.findNearestWeakerEnemy(gameData);
+     return healthWell;
+   }
+   else if (typeof weakEnemy != "undefined") {
+     return weakEnemy;
+   }
+   else if (typeof directionToEnemy != "undefined" && distanceToEnemy === 1) {
+     return directionToEnemy;
+   }
+   else if (typeof nonTeamDiamondMine != "undefined" && distanceToEnemy > 3 ){
+     return nonTeamDiamondMine;
+   }
+//   else if (typeof teamMember != "undefined") {
+//     return teamMember;
+//   }
+   else {
+     return healthWell;
    }
 };
 

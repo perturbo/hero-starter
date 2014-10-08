bb1710605796426627932eabfc501df16265c796
Peter Beverwyk <pete@bwyk.com>
2014-10-07 22:10:22 -0500

    Added comments to describe reasons for each move

--- hero.js
+++ hero.js
@@ -1,5 +1,5 @@
-// // The "Careful Assassin"
-// // This hero will attempt to kill the closest weaker enemy hero.
+// The "Careful grab-a-mine-if-i-can Assassin"
+//
 var move = function(gameData, helpers) {
    var myHero = gameData.activeHero;
 
@@ -10,27 +10,45 @@ var move = function(gameData, helpers) {
    });
    var distanceToEnemy = enemyStats.distance;
    var directionToEnemy = enemyStats.direction;
+   var healthOfEnemy = enemyStats.health;
 
    weakEnemy = helpers.findNearestWeakerEnemy(gameData);
    nonTeamDiamondMine = helpers.findNearestNonTeamDiamondMine(gameData);
    healthWell = helpers.findNearestHealthWell(gameData);
-   teamMember = helpers.findNearestTeamMember(gameData);
+//   teamMember = helpers.findNearestTeamMember(gameData);
+
    
-   if (myHero.health < 50) {
+   // Finish him!!
+   if (typeof directionToEnemy != "undefined" && distanceToEnemy === 1 && healthOfEnemy <= 30) {
+     return directionToEnemy;
+   }
+
+   // OMG RUN....
+   else if (myHero.health < 40) {
      return healthWell;
    }
+
+   // Shhh hunting rabbit
    else if (typeof weakEnemy != "undefined") {
      return weakEnemy;
    }
+
+   // While I'm here ....
    else if (typeof directionToEnemy != "undefined" && distanceToEnemy === 1) {
      return directionToEnemy;
    }
+
+   // Quick!  No one is around to stop me
    else if (typeof nonTeamDiamondMine != "undefined" && distanceToEnemy > 3 ){
      return nonTeamDiamondMine;
    }
+
+// Help a friend
 //   else if (typeof teamMember != "undefined") {
 //     return teamMember;
 //   }
+
+   // This looks like a good place to sit .. except when I'm at 100 health [TODO]
    else {
      return healthWell;
    }
